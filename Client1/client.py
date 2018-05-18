40998d1d1073f08e9bf0bf961ce5d302e3a97261
Charles Bucher <charles.abucher@gmail.com>
2018-05-18 03:07:28 -0700

    Added error checking & new-user setup
    try/except clauses for closed sockets and keyboard interrupts.
    
    Also, skeleton added for setting up a new user.

--- Client1/client.py
+++ Client1/client.py
@@ -41,187 +41,248 @@ def main():
     serverPort = int(sys.argv[2])
     privKeyFile = sys.argv[3]
 
-    primarySocket = createSocket(serverPort)
+    try:
+        primarySocket = createSocket(serverPort)
+    except ConnectionRefusedError:
+        print("Server is currently not accepting requests. Try again later.")
+        exit(0)
+
+    # Welcome Screen Loop
+    print(idt,idt, "\n\nWelcome to Order Processing!\n")
+    try:
+        while True:
+            exists = input("Are you a returning user? (Y or N): ")
+
+            if exists not in ['Y', 'y', 'N', 'n']:
+                print("Please enter Y or N")
+                continue
+            
+            if exists in ['Y', 'y']: # Go straight to login
+                break
+            
+            print("\nPlease enter the following information:")
+            new_userName = input("Username: ")
+            new_email = input("Email: ")
+            passwordsVerify = False
+            while not passwordsVerify:
+                new_password1 = getpass.getpass("Password: ")
+                new_password2 = getpass.getpass("Confirm Password: ")
+                if new_password1 != new_password2:
+                    print("Passwords don't match! Please retry")
+                    continue
+                else:
+                    passwordsVerify = True
+            
+            print("\nFinally, the server needs a public key to identify you by.")
+            publicKeyVerify = False
+            while not publicKeyVerify:
+                generate = input("Do you want the system to generate a public key for you? (Y or N): ")
+                if exists in ['Y', 'y']:
+                    print("Wonderful! You have been setup as a new user! Proceed to login")
+                    publicKeyVerify
+                
+                publicKey = input("Input the '.pem' file containing your public key: ")
+                if publicKey[-4:] != ".sl3" or not os.path.isfile(publicKey):
+                    print("Public key file is invalid.")
+
+            if publicKeyVerify and passwordsVerify:
+                break
+    except KeyboardInterrupt:
+        print()
+        exit(0)
 
     authenticated = False
     userName = ""
 
+    print("\nPlease enter your credentials.")
+
     while True:
-		
-        # Perform initial authentication process
-        while not authenticated:
-            userInput = input("Username: ")
-            userName = userInput
-            passInput = getpass.getpass("Password: ")
 
-            # Hash password and send over TCP
-            sendMsg(primarySocket, userInput)
-            sendMsg(primarySocket, passInput)
+        try:
+            # Perform initial authentication process
+            while not authenticated:
+                userInput = input("Username: ")
+                userName = userInput
+                passInput = getpass.getpass("Password: ")
 
-            # Receive authentication success/failure from server
-            serverMsg = recvMsg(primarySocket)
-            parsedMsg = serverMsg.split('::')
+                # Hash password and send over TCP
+                sendMsg(primarySocket, userInput)
+                sendMsg(primarySocket, passInput)
 
-            if str(parsedMsg[0]) == "1":
-                authenticated = True
-                print(idt, "Authentication successful.\n")
-                print(idt, "Enter command choice:")
-                print(idt, "test - test connection")
-                print(idt, "pwd - change password")
-                print(idt, "order - create an order")
-                print(idt, "inventory - search for an item in inventory")
-                print(idt, "quit - quit and close server")
-            else:
-                errorMsg = parsedMsg[1]
-                print(idt, errorMsg)
-            continue
+                # Receive authentication success/failure from server
+                serverMsg = recvMsg(primarySocket)
+                parsedMsg = serverMsg.split('::')
 
-        ans = input("$> ")
-
-        # Argument counting using spaces
-        arg_count = ans.count(" ")
-
-        if arg_count == 1:
-            print(idt, "Invalid command. Try: 'test', 'order', 'inventory', 'pwd', or 'quit'")
-        elif arg_count == 0:
-            command = ans
-
-        # Send the command to server
-        sendMsg(primarySocket, ans)
-
-        # Process input
-        if command == "test":
-
-            testMsg = "Hello Server :)"
-
-            sendMsg(primarySocket, testMsg)
-            print(idt, "Sent test message to server")
-            recvMsg(primarySocket)
-            print(idt, "Received test message from server")
-            print(idt, "Connection test successful")
-
-        elif command == "pwd":
-            # Update password
-
-            # 1. Prompt for current password
-            oldPassword = getpass.getpass("Enter old password: ")
-            oldPassword = SHA512.new(oldPassword.encode(codingMethod)).hexdigest()
-
-            # 2. Send to server for verification
-            sendMsg(primarySocket, oldPassword)
-            passwordIsCorrect = recvMsg(primarySocket)
-
-            # 3. If password match, continue. Else, notify server that command is terminated
-            if passwordIsCorrect != "1":
-                print(idt, "Old password incorrect!")
+                if str(parsedMsg[0]) == "1":
+                    authenticated = True
+                    print(idt, "Authentication successful.\n")
+                    print(idt, "Enter command choice:")
+                    print(idt, "test - test connection")
+                    print(idt, "pwd - change password")
+                    print(idt, "order - create an order")
+                    print(idt, "inventory - search for an item in inventory")
+                    print(idt, "quit - quit and close server")
+                else:
+                    errorMsg = parsedMsg[1]
+                    print(idt, errorMsg)
                 continue
 
-            # 4. Get new password information and send to server
-            toSendMsg = ""
+            ans = input("$> ")
 
-            # Prompt for new password/confirmation.
-            newPassword1 = getpass.getpass("Enter new password: ")
-            newPassword2 = getpass.getpass("Confirm new password: ")
-            if newPassword1 != newPassword2:
-                print(idt, "New passwords do not match!")
-                toSendMsg = "0::"
-            else:
-                # Prepare to send server new password hash
-                toSendMsg = "1::" + SHA512.new(newPassword1.encode(codingMethod)).hexdigest()
+            # Argument counting using spaces
+            arg_count = ans.count(" ")
 
-            sendMsg(primarySocket, toSendMsg)
+            if arg_count == 1:
+                print(idt, "Invalid command. Try: 'test', 'order', 'inventory', 'pwd', or 'quit'")
+            elif arg_count == 0:
+                command = ans
 
-            # 5. Receive success msg if we attempted to update
-            if toSendMsg[0] == "1":
-                flag = recvMsg(primarySocket)
+            # Send the command to server
+            sendMsg(primarySocket, ans)
 
-                if str(flag) != "1":
-                    print(idt, "Issue updating password")
+            # Process input
+            if command == "test":
+
+                testMsg = "Hello Server :)"
+
+                sendMsg(primarySocket, testMsg)
+                print(idt, "Sent test message to server")
+                recvMsg(primarySocket)
+                print(idt, "Received test message from server")
+                print(idt, "Connection test successful")
+
+            elif command == "pwd":
+                # Update password
+
+                # 1. Prompt for current password
+                oldPassword = getpass.getpass("Enter old password: ")
+                oldPassword = SHA512.new(oldPassword.encode(codingMethod)).hexdigest()
+
+                # 2. Send to server for verification
+                sendMsg(primarySocket, oldPassword)
+                passwordIsCorrect = recvMsg(primarySocket)
+
+                # 3. If password match, continue. Else, notify server that command is terminated
+                if passwordIsCorrect != "1":
+                    print(idt, "Old password incorrect!")
+                    continue
+
+                # 4. Get new password information and send to server
+                toSendMsg = ""
+
+                # Prompt for new password/confirmation.
+                newPassword1 = getpass.getpass("Enter new password: ")
+                newPassword2 = getpass.getpass("Confirm new password: ")
+                if newPassword1 != newPassword2:
+                    print(idt, "New passwords do not match!")
+                    toSendMsg = "0::"
                 else:
-                    print(idt, "Password updated successfully!")
+                    # Prepare to send server new password hash
+                    toSendMsg = "1::" + SHA512.new(newPassword1.encode(codingMethod)).hexdigest()
 
-        elif command == "order":
-            # Order protocol...
-            # Get order information from the user
-            # Send the order message to server
-            # Receive order success/failure message from server
-            
-            orderDesc = input("\nWhat do you want to order? ")
-            orderQty = int(input("How many? "))
+                sendMsg(primarySocket, toSendMsg)
 
-            theOrder = Order(initList=[orderDesc, orderQty, int(float(datetime.utcnow().timestamp())), userName])
-            
-            # Get the private key from the input parameters
-            privKey = load_key(load_sig(privKeyFile))
+                # 5. Receive success msg if we attempted to update
+                if toSendMsg[0] == "1":
+                    flag = recvMsg(primarySocket)
 
-            # Get the signature of the order
-            orderSignature = getFileSig(str(theOrder), privKey, isFile=False)
-            
-            # Now, encrypt the order itself
-            # First, generate an AES key and initialization vector for block-chain encryption.
-            aesKey = os.urandom(16) # 16 random bytes
-            iv = Random.new().read(AES.block_size) # Random IV
-            myEncryptor = AESCipher(aesKey, iv)
+                    if str(flag) != "1":
+                        print(idt, "Issue updating password")
+                    else:
+                        print(idt, "Password updated successfully!")
 
-            # Second, generate the encrypted text.
-            encryptedOrder = myEncryptor.encrypt(str(theOrder))
+            elif command == "order":
+                # Order protocol...
+                # Get order information from the user
+                # Send the order message to server
+                # Receive order success/failure message from server
+                
+                orderDesc = input("\nWhat do you want to order? ")
+                orderQty = int(input("How many? "))
 
-            # Send a sequence of messages in this order:
-            #   1. Digital signature
-            #   2. AES key
-            #   3. IV vector
-            #   4. Encrypted order
-            # Note: Messages 2-4 are byte-strings and cannot be encoded/decoded.
+                theOrder = Order(initList=[orderDesc, orderQty, int(float(datetime.utcnow().timestamp())), userName])
+                
+                # Get the private key from the input parameters
+                privKey = load_key(load_sig(privKeyFile))
 
-            sendMsg(primarySocket, orderSignature, encode=False)
+                # Get the signature of the order
+                orderSignature = getFileSig(str(theOrder), privKey, isFile=False)
+                
+                # Now, encrypt the order itself
+                # First, generate an AES key and initialization vector for block-chain encryption.
+                aesKey = os.urandom(16) # 16 random bytes
+                iv = Random.new().read(AES.block_size) # Random IV
+                myEncryptor = AESCipher(aesKey, iv)
 
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            # TODO: Encrypt AES key & iv using RSA.
+                # Second, generate the encrypted text.
+                encryptedOrder = myEncryptor.encrypt(str(theOrder))
 
-            sendMsg(primarySocket, aesKey, encode=False)
-            sendMsg(primarySocket, iv, encode=False)
+                # Send a sequence of messages in this order:
+                #   1. Digital signature
+                #   2. AES key
+                #   3. IV vector
+                #   4. Encrypted order
+                # Note: Messages 2-4 are byte-strings and cannot be encoded/decoded.
 
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            ###################################################################
-            sendMsg(primarySocket, encryptedOrder, encode=False)
+                sendMsg(primarySocket, orderSignature, encode=False)
 
-            print("Order sent to server. Waiting for reply.....")
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                # TODO: Encrypt AES key & iv using RSA.
 
-            # Receive order confirmation/failure notice from server
-            serverMsg = recvMsg(primarySocket)
-            print(idt, serverMsg.split("::")[1])
+                sendMsg(primarySocket, aesKey, encode=False)
+                sendMsg(primarySocket, iv, encode=False)
 
-        elif command == "inventory":
-            item = input("Which item are you looking for? ")
-            sendMsg(primarySocket, item)
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                ###################################################################
+                sendMsg(primarySocket, encryptedOrder, encode=False)
 
-            response = recvMsg(primarySocket)
-            print(idt, response.split("::")[1])
+                print("Order sent to server. Waiting for reply.....")
 
-        elif command == "quit":
-            print(idt, "Closing now")
+                # Receive order confirmation/failure notice from server
+                serverMsg = recvMsg(primarySocket)
+                print(idt, serverMsg.split("::")[1])
 
-            primarySocket.close()
-            break
-        else:
-            print(idt, "Invalid command. Try: 'test', 'order', 'inventory', 'pwd', or 'quit'")
+            elif command == "inventory":
+                item = input("Which item are you looking for? ")
+                sendMsg(primarySocket, item)
+
+                response = recvMsg(primarySocket)
+                print(idt, response.split("::")[1])
+
+            elif command == "quit":
+                print(idt, "Closing now")
+
+                primarySocket.close()
+                break
+            else:
+                print(idt, "Invalid command. Try: 'test', 'order', 'inventory', 'pwd', or 'quit'")
+        except(ConnectionResetError, BrokenPipeError):
+            print("Connection terminated by the server. Try again later.")
+            exit(0)
+        except KeyboardInterrupt:
+            if authenticated:
+                print("\r\rPlease ensure use the 'quit' method instead of keyboard interrupt.")
+            else:
+                print()
+                primarySocket.close()
+                exit(0)
 
 
 if __name__ == "__main__":
